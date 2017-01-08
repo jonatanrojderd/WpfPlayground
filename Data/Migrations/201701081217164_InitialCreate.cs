8a269a7b48281c78aafdbe66d80c3d55b0e0983d
Valentine\ADHD <jonatanrojderd@gmail.com>
2017-01-08 13:52:25 +0100

    Enabled migrations (Code First).

---
+++ Data/Migrations/201701081217164_InitialCreate.cs
@@ -0,0 +1,27 @@
+namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.People",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.People");
+        }
+    }
+}
