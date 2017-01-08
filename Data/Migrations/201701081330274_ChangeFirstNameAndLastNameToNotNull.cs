54972b07dde7ca31aec3162fa1553169272cd616
Valentine\ADHD <jonatanrojderd@gmail.com>
2017-01-08 14:32:25 +0100

    Updated FirstName and LastName properties to be NotNull in database.

---
+++ Data/Migrations/201701081330274_ChangeFirstNameAndLastNameToNotNull.cs
@@ -0,0 +1,19 @@
+namespace Data.Migrations
+{
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangeFirstNameAndLastNameToNotNull : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.People", "FirstName", c => c.String(nullable: false));
+            AlterColumn("dbo.People", "LastName", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.People", "FirstName", c => c.String());
+            AlterColumn("dbo.People", "LastName", c => c.String());
+        }
+    }
+}
